c8b35de0bd11e3bea2ebe68bd6acd88ecdf44f82
reidelaspessoa <107411087+reidelaspessoa@users.noreply.github.com>
2024-03-19 08:52:42 -0300

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-console.log(botoes);
-
  
 for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
 
@@ -23,41 +21,38 @@ const contadores = document.querySelectorAll(".contador");
 const tempos =
 [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempos[0]);
-contadores[1].textContent = calculaTempo(tempos[1]);
-contadores[2].textContent = calculaTempo(tempos[2]);
-contadores[3].textContent = calculaTempo(tempos[3]);
+
+function calculaTempo(tempoObjetivo) { 
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+  
+    if (tempoFinal > 0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {
+return "Prazo Finalizado";
+    }
+}
+
 
 function atualizaCronometro(){
-for (let i = 0; i < contadores.length; i++) {
+for (let i=0; i<contadores.length;i++) {
 contadores[i].textContent = calculaTempo(tempos[i]);
 }
 }
 
 function comecaCronometro(){
 atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
+setInterval(atualizaCronometro,1000);
 }
 //comecaCronometro();
-    
-    function calculaTempo(tempoObjetivo) { 
-        let tempoAtual = new Date();
-        let tempoFinal = tempoObjetivo - tempoAtual;
-        let segundos = Math.floor(tempoFinal / 1000);
-        let minutos = Math.floor(segundos / 60);
-        let horas = Math.floor(minutos / 60);
-        let dias = Math.floor(horas / 24);
-
-        segundos %= 60;
-        minutos %= 60;
-        horas %= 24;
-      
-        if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
-        } else {
-return "Prazo Finalizado";
-        }
-    }
     
 
     
