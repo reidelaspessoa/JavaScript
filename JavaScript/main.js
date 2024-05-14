b735cf22d92035f3c9142a95720883e9992ddcbc
reidelaspessoa <107411087+reidelaspessoa@users.noreply.github.com>
2024-05-14 09:29:47 -0300

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -17,8 +17,9 @@ const contadores = document.querySelectorAll(".contador");
     const tempoObjetivo2 = new Date("2024-12-05T00:00:00");  
     const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
     const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+    const tempoObjetivo5 = new Date("2025-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4, tempoObjetivo5];
 
 
 function calculaTempo(tempoObjetivo) { 
